880d16f9875627a857949e5bb56b6189c702b9ef
jotish03 <jotishsharma03@gmail.com>
2023-10-03 18:16:03 +0530

    react-router addded

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import {Card} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import Rating from './Rating'
 
 const Product = ({eproduct}) => {
   return (
     <Card className='my-3 p-3 rounded'>
-        <a href={`/product/${eproduct._id}`}>
+        <Link to={`/product/${eproduct._id}`}>
             <Card.Img src={eproduct.image} variant="top" />
                 
        
 
-        </a>
+        </Link>
    <Card.Body>
-   <a href={`/product/${eproduct._id}`}>
+   <Link to={`/product/${eproduct._id}`}>
         <Card.Title as="div"><strong>{eproduct.name}</strong></Card.Title>
-</a>
+</Link>
+<Card.Text as='div'>
+    <Rating value={eproduct.rating} text={`${eproduct.numReviews} reviews`} />
+</Card.Text>
         <Card.Text as="h3">
 
             {eproduct.price}
