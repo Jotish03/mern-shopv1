43353b158f8cdb824e41a895db18281d3cc06f02
Jotish Sharma <jotishsharma03@gmail.com>
2023-10-10 23:49:48 +0530

    product fetched

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import products from "./data/products.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 
-const port = 5001;
+const port = process.env.PORT || 1003;
 
 app.get("/", (req, res) => {
   res.send("Server Index");
@@ -10,6 +13,11 @@ app.get("/", (req, res) => {
 
 app.get("/api/products", (req, res) => {
   res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((item) => item._id === req.params.id);
+  res.json(product);
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
